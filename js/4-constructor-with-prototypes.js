96b940640d940ec14a13b76aa1287c86cfe66817
Victor Pinedo <=victorpinedomartinez@gmail.com>
2021-08-08 13:29:12 -0500

    Constructor Using Prototype

--- js/4-constructor-with-prototypes.js
+++ js/4-constructor-with-prototypes.js
@@ -3,12 +3,12 @@ function Car(model, year, miles) {
     this.model = model;
     this.year = year;
     this.miles = miles;
-
-    this.toString = () => {
-        const message = `${this.model} has done ${this.miles} miles.`;
-        return message;
-    };
 }
+
+Car.prototype.toString = () => {
+    const message = `${this.model} has done ${this.miles} miles.`;
+    return message;
+};
 
 // Usage
 var civiv = new Car("Honda Civiv", 2009, 20000);
