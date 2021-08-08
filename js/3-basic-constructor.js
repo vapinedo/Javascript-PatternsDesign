12d18c2c1173f29e9d46df49228df9da3ebc2748
Victor Pinedo <=victorpinedomartinez@gmail.com>
2021-08-08 13:17:28 -0500

    Basic Constructor Example

---
+++ js/3-basic-constructor.js
@@ -0,0 +1,16 @@
+
+function Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+
+    this.toString = function() {
+        const message = `${this.model} has done ${this.miles} miles.`;
+        return message;
+    };
+}
+
+// Usage
+var civiv = new Car("Honda Civiv", 2009, 20000);
+console.log(civiv);
+console.log(civiv.toString());
